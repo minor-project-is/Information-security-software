f2e4aaa23d5e235bb1638f78e4078af0ed1a9c2a
Debasispothal <35737072+Debasispothal@users.noreply.github.com>
2019-01-28 22:04:48 +0530

    intial file
    commit

---
+++ Read_me_please.txt
@@ -0,0 +1,2 @@
+*** copy the file to the netbeans project and start working
+***We will be working for 5 Encryption algo any further ideas are welcomed.
---
+++ information security software/src/information/security/software/InformationSecuritySoftware.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package information.security.software;
+
+/**
+ *
+ * @author 1602366
+ */
+public class InformationSecuritySoftware {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        input i=new input();
+        i.setLocationRelativeTo(null);
+        i.setVisible(true);
+        
+    }
+  
+    
+}
